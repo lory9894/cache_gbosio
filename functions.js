31d13d9a01fe53fac6fac0fc3de1e975a1e7524e
Lorenzo Dentis <lory9894@gmail.com>
2023-09-19 17:52:54 +0200

    now time is displayed

--- functions.js
+++ functions.js
@@ -1,8 +1,25 @@
 let id;
 
+const options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+
 window.onload = function() {
+    $.ajax({
+        url: "https://worldtimeapi.org/api/ip",
+        type: "GET",
+        dataType: "json",
+        success: function (data) {
+            checkTime(data);
+        }
+    });
     id = GetURLParameter('id');
-    showCoord(null,null, id);
+    id_given = showRobot(id);
+    navigator.geolocation.getCurrentPosition(success, error, options);
+    if (id_given)
+        showCoord(null,null);
 }
 function GetURLParameter(sParam) {
     var sPageURL = window.location.search.substring(1);
@@ -17,11 +34,32 @@ function GetURLParameter(sParam) {
     }
 }
 
-function showCoord(ip, time, id) {
-    console.log(ip, time, id);
+function showRobot(id) {
     if (id >= 1 && id <= 8) {
     $("#robot").attr("src","images/"+ id + ".jpeg");
     }else{
         //todo: errore
+        return false;
     }
+    return true;
+}
+
+function checkTime(data){
+    var ora = data.datetime.split("T")[1].split(":")[0];
+    $("#time").html("Sono le ore " + ora );
+    if (ora >= 22 || ora <= 5) {
+
+    }
+}
+
+function success(pos) {
+  const crd = pos.coords;
+
+  console.log('Your current position is:');
+  console.log(`Latitude : ${crd.latitude}`);
+  console.log(`Longitude: ${crd.longitude}`);
+  console.log(`More or less ${crd.accuracy} meters.`);
+}
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
 }
